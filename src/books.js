8ae25e923ef7ef0ae8a3a5e28d227339da67f867
thadeus <thadeusnyariki94@gmail.com>
2022-09-14 22:13:12 +0300

    Ongoing

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    id: 1,
+    img: 'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/8144Vic9C5L._AC_UL600_SR600,400_.jpg',
+    title: 'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth',
+  },
+  {
+    id: 2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81EVdWdmOKL._AC_UL300_SR300,200_.jpg',
+    title: 'Brown Bear, Brown Bear, What Do You See?',
+    author: 'Bill Martin Jr.',
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/81HE+JDes-L._AC_UL600_SR600,400_.jpg',
+    title: 'The Simply Happy Cookbook',
+    author: 'Kathy Doocy',
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// CSS
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import {books} from './books.js'
+import Book from './Book.js'
+
+function Booklist() {
+  return (
+    <section className='booklist'>
+      {books.map((book) => {
+        const { img, tile, author } = book;
+        return <Book key={book.id} book={book} />;
+      })}
+    </section>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(Booklist());
